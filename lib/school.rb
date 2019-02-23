0154b79d14dbd46c5aa8ce20f5c05e59426fc2bc
Kylla Harris <kylla.harris@gmail.com>
2019-02-23 18:36:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,12 @@
-# code here!
+class School
+  attr_accessor :roster, :add_student
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student, grade)
+    
+end
+end
